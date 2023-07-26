50dd4e0ca2a323d34eabcc283a4dcf663b3c99c8
M-Ali239 <140588411+M-Ali239@users.noreply.github.com>
2023-07-26 02:18:19 -0700

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,21 @@
+var message = "Hey Abu Hurairah, I have started learning TYPESCRIPT.";
+var nam = "MUHAMMAD ALI";
+var a1 = 1;
+var a2 = 2;
+var a3 = 3;
+var a4 = 4;
+var a5 = 5;
+var a6 = 6;
+var a7 = 7;
+var a8 = 8;
+var a9 = 9;
+var a10 = 10;
+console.log(message);
+console.log(nam);
+function add(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) { return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10; }
+add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+function subtract(a10, a8) { return a10 - a8; }
+;
+subtract(10, 8);
+console.log(subtract(10, 8));
---
+++ test1.ts
@@ -0,0 +1,24 @@
+let message:string="Hey Abu Hurairah, I have started learning TYPESCRIPT."; 
+
+let nam:string="MUHAMMAD ALI";
+
+let a1:number=1;
+let a2:number=2;
+let a3:number=3;
+let a4:number=4;
+let a5:number=5;
+let a6:number=6;
+let a7:number=7;
+let a8:number=8;
+let a9:number=9;
+let a10:number=10;
+
+console.log(message);
+console.log(nam);
+function add(a1:number,a2:number,a3:number,a4:number,a5:number,a6:number,a7:number,a8:number,a9:number,a10:number):number{return a1+a2+a3+a4+a5+a6+a7+a8+a9+a10;}
+add(1,2,3,4,5,6,7,8,9,10);
+console.log(add(1,2,3,4,5,6,7,8,9,10));
+
+function subtract(a10:number , a8:number):number{return a10-a8};
+subtract(10,8);
+console.log(subtract(10,8));
